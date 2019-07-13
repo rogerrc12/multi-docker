73d1c06f00a7290df6e124c1963aeb7d9803a2ee
Roger Rengifo <rogerreng.c@gmail.com>
2019-07-12 21:00:03 -0500

    adding style changes

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,9 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <h1 className="App-title">Fib Calculator</h1>
+            <Link to="/" className="link-nav">Home</Link>
+            <Link to="/otherpage" className="link-nav">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
